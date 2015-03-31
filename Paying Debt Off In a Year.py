a15c4409ca11624128b5bbd99b294f55d02f78a3
MizusiroHaruka <mizusiroharuka@gmail.com>
2015-03-31 20:30:17 +0800

    Revert "homework"
    This reverts commit 823a34f3660aec996dcd68d37ecb10be9ffbd246.

--- Paying Debt Off In a Year.py
+++
@@ -1,5 +0,0 @@
-balance = 999999
-annualInterestRate = 0.18
-q=1+annualInterestRate/12
-Low=((q**11*(1-q))/(1-q**12))*balance
-print("Lowest Payment: "+str(round(Low,2)))
--- bob.py
+++
@@ -1,8 +0,0 @@
-s=raw_input("please input a string:")
-count=0
-start=0
-end=len(s)
-while s.find('bob',start,end)!=-1:
-    start=s.find('bob',start,end)+2
-    count+=1
-print("Number of vowels: "+str(count))
